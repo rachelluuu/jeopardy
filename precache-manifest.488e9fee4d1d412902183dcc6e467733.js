87b53620c14f9275f061d5152799f4f3b355fe3c
Rachel Lu <rbl83@cornell.edu>
2019-10-13 18:01:27 -0400

    Updates

---
+++ precache-manifest.488e9fee4d1d412902183dcc6e467733.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b51636d9e976f42566e0661abc299b2b",
+    "url": "/jeopardy/index.html"
+  },
+  {
+    "revision": "9d7125f9b661cefed275",
+    "url": "/jeopardy/static/css/2.a8360274.chunk.css"
+  },
+  {
+    "revision": "9f7db09fdb20fabd0775",
+    "url": "/jeopardy/static/css/main.e654fce1.chunk.css"
+  },
+  {
+    "revision": "9d7125f9b661cefed275",
+    "url": "/jeopardy/static/js/2.b1853ec6.chunk.js"
+  },
+  {
+    "revision": "9f7db09fdb20fabd0775",
+    "url": "/jeopardy/static/js/main.fd2922ea.chunk.js"
+  },
+  {
+    "revision": "bc200913a67035b81764",
+    "url": "/jeopardy/static/js/runtime-main.a21f22d2.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/jeopardy/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/jeopardy/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/jeopardy/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/jeopardy/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/jeopardy/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
