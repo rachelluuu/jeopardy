683257b14bdea93c86b621786af781383f5b6f96
Rachel Lu <rbl83@cornell.edu>
2019-10-13 23:46:04 -0400

    aesthetics

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -3,11 +3,11 @@ import React from 'react';
 function Footer(props) {
     return (
         <div className="footer">
-                <div className="row justify-content-center">             
-                    <div className="col-auto">
-                        <p>© Copyright 2019 Rachel Lu</p>
-                    </div>
+            <div className="row justify-content-center">
+                <div className="col-auto">
+                    <p> Developed by Rachel Lu</p>
                 </div>
+            </div>
         </div>
     )
 }
--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -3,12 +3,17 @@ import { Card, CardBody, CardTitle } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
 
-function RenderQAItem ({qa, onClick}) {
+function RenderQAItem({ qa, onClick }) {
     return (
         <Card>
             <Link to={`/search/${qa.id}`} >
+                <center>
+                    <CardTitle>
+                        {qa.category.title}
+                    </CardTitle>
+                </center>
                 <CardBody>
-                    <CardTitle>{qa.question}</CardTitle>
+                    {qa.question}
                 </CardBody>
             </Link>
         </Card>
@@ -17,28 +22,28 @@ function RenderQAItem ({qa, onClick}) {
 
 const Search = (props) => {
     const results = props.qas.qas
-        .filter((qa) => qa.question!=='')
+        .filter((qa) => qa.question !== '')
         .map((qa) => {
-        return (
-            <div className="question col-lg-3 col-md-6 col-12"  key={qa.id}>
-                <RenderQAItem qa={qa} onClick={props.onClick} />
-            </div>
-        );
-    });
+            return (
+                <div className="question col-lg-3 col-md-6 col-12" key={qa.id}>
+                    <RenderQAItem qa={qa} onClick={props.onClick} />
+                </div>
+            );
+        });
 
     if (props.qas.isLoading) {
-        return(
+        return (
             <div className="container">
-                <div className="xxxrow">            
+                <div className="xxxrow">
                     <Loading />
                 </div>
             </div>
         );
     }
     if (props.qas.errMess) {
-        return(
+        return (
             <div className="container">
-                <div className="row"> 
+                <div className="row">
                     <div className="col-12">
                         <h4>{props.qas.errMess}</h4>
                     </div>
