1199c5feffd978d9e6bfaf75b7e65cf2719e89c5
Rachel Lu <rbl83@cornell.edu>
2019-10-13 21:40:54 -0400

    replace BrowserRouter w/ HashRouter

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import 'font-awesome/css/font-awesome.css';
 import 'bootstrap-social/bootstrap-social.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
-ReactDOM.render(<BrowserRouter basename={process.env.PUBLIC_URL}>
-  <App /></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<HashRouter basename={process.env.PUBLIC_URL}>
+  <App /></HashRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
