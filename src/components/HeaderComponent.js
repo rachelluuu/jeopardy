1ded60f1f818fcb57af9000057796af0cc6ccffe
Rachel Lu <rbl83@cornell.edu>
2019-10-13 22:01:49 -0400

    Redirect Jeopardy! Header to /search

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -13,25 +13,25 @@ class Header extends Component {
 
     toggleNav() {
         this.setState({
-          isNavOpen: !this.state.isNavOpen
+            isNavOpen: !this.state.isNavOpen
         });
     }
 
     render() {
-        return(
+        return (
             <div>
                 <Navbar dark expand="md">
                     <div className="container">
                         <NavbarToggler onClick={this.toggleNav} />
-                        <NavbarBrand className="mr-auto" href="/"><b>Jeopardy!</b></NavbarBrand>
+                        <NavbarBrand className="mr-auto" href="/search"><b>Jeopardy!</b></NavbarBrand>
                         <Collapse isOpen={this.state.isNavOpen} navbar>
                             <Nav navbar>
-                            <NavItem>
-                                <NavLink className="nav-link" to='/search'><span className="fa fa-info fa-lg"></span> Search</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link"  to='/play'><span className="fa fa-list fa-lg"></span> Play</NavLink>
-                            </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to='/search'><span className="fa fa-info fa-lg"></span> Search</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to='/play'><span className="fa fa-list fa-lg"></span> Play</NavLink>
+                                </NavItem>
                             </Nav>
                         </Collapse>
                     </div>
