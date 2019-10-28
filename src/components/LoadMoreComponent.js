ad5b6dbd8022562ac0371b8a1bc349e1872a8e62
Rachel Lu <rbl83@cornell.edu>
2019-10-27 21:13:52 -0400

    fix issue with loading more

--- src/components/LoadMoreComponent.js
+++ src/components/LoadMoreComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button } from 'reactstrap';
 
+// Get more results if user clicks "More..." at the bottom of search results
 class LoadMore extends React.Component {
   constructor(props) {
     super(props);
@@ -9,15 +10,15 @@ class LoadMore extends React.Component {
 
   clicked(e) {
     e.preventDefault();
-    const f = {...this.props.appProps.qas};
-    f.offset = f.offset ? f.offset+100 : 100;
+    const f = { ...this.props.appProps.qas };
+    f.offset = f.offset ? f.offset + 100 : 100;
     this.props.appProps.fetchQAs(f);
   }
 
   render() {
     return (
-      <Button className="moreButton" onClick={this.clicked} disabled={this.props.appProps.qas.isLoading} > 
-        {this.props.appProps.qas.isLoading ? "Loading..." : "More..." }
+      <Button className="moreButton" onClick={this.clicked} disabled={this.props.appProps.qas.isLoading} >
+        {this.props.appProps.qas.isLoading ? "Loading..." : "More..."}
       </Button>
     );
   }
